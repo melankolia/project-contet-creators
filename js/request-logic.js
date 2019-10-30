4ba0360ecf3ebaf98abe4630ab14b73d09cdecea
melankolia <setyo.nugroho96@gmail.com>
2019-10-30 15:52:13 +0700

    penambahan rules di dalam getContentType()

--- js/request-logic.js
+++ js/request-logic.js
@@ -4,5 +4,19 @@
 
 
 function getContentType(filename) {
-  const extension = filename.match(/.*\.([^\.]*)$/)[1];
+  const extension = filename.match(/.*\.([^\.]*)$/)[1]; 
+  if (extension === 'html'){
+    return 'text/html';
+  }
+  if (extension === 'css'){
+    return 'text/css';
+  }
+  if (extension === 'jpeg' || extension === 'jpg'){
+    return 'image/jpeg';
+  }
+  else {
+    return 'text/plain';
+  }
+  
 }
+
